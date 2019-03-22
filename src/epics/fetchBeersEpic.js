b3aa307d8b36624a77cfc1b59f0bd73c00660972
Michael Trang <michael.trang@hotmail.com>
2019-03-22 23:32:06 +1100

    add cancel from keyboard or click with regular ajax request actions

--- src/epics/fetchBeersEpic.js
+++ src/epics/fetchBeersEpic.js
@@ -1,24 +1,26 @@
 import {ajax} from 'rxjs/ajax'
 import {
   map,
+  mapTo,
   delay,
   switchMap,
   debounceTime,
   filter,
   catchError,
-  takeUntil,
+  // takeUntil,
 } from 'rxjs/operators'
 // import {map, switchMap, tap, ignoreElements} from 'rxjs/operators'
 import {
   SEARCH,
+  CANCEL,
+  reset,
   fetchFulfilled,
   fetchFailed,
   setStatus,
-  CANCEL,
   // FETCH_DATA,
 } from '../reducers/beersAction'
 import {ofType} from 'redux-observable'
-import {concat, of} from 'rxjs'
+import {concat, of, fromEvent, merge, race} from 'rxjs'
 
 const API = 'https://api.punkapi.com/v2/beers'
 const searchQuery = term => `${API}?beer_name=${encodeURIComponent(term)}`
@@ -29,18 +31,34 @@ export function fetchBeersEpic(action$) {
     debounceTime(500),
     // filter(({payload}) => payload.trim() !== ''),
     switchMap(({payload}) => {
-      return concat(
-        of(setStatus('pending')),
-        ajax.getJSON(searchQuery(payload)).pipe(
-          delay(5000), // simulate network busy, delay
-          // accepts any observable until CANCEL, then unsubscribe to the ajax request
-          takeUntil(action$.pipe(ofType(CANCEL))),
-          map(fetchFulfilled),
-          catchError(error => {
-            return of(fetchFailed(error.response.message))
-          }),
-        ),
+      const ajax$ = ajax.getJSON(searchQuery(payload)).pipe(
+        delay(5000), // simulate network delay
+        map(fetchFulfilled),
+        catchError(error => {
+          return of(fetchFailed(error.response.message))
+        }),
       )
+
+      const cancel$ = merge(
+        action$.pipe(ofType(CANCEL)),
+        fromEvent(document, 'keyup').pipe(
+          filter(e => e.key === 'Escape' || e.key === 'Esc'),
+        ),
+      ).pipe(mapTo(reset()))
+
+      return concat(of(setStatus('pending')), race(ajax$, cancel$))
+      // return concat(
+      //   of(setStatus('pending')),
+      //   ajax.getJSON(searchQuery(payload)).pipe(
+      //     delay(5000), // simulate network busy, delay
+      //     // accepts any observable until CANCEL, then unsubscribe to the ajax request
+      //     takeUntil(action$.pipe(ofType(CANCEL))),
+      //     map(fetchFulfilled),
+      //     catchError(error => {
+      //       return of(fetchFailed(error.response.message))
+      //     }),
+      //   ),
+      // )
     }),
     // ofType(FETCH_DATA),
     // switchMap(() => {
